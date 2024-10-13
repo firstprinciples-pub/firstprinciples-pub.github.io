49a1fe70fa573565a902ab903aeaec874c1463ff
ml5885 <limichael353@gmail.com>
2024-10-12 22:54:37 -0700

    updated date

--- src/data/blogPosts.tsx
+++ src/data/blogPosts.tsx
@@ -9,7 +9,7 @@ export const blogPosts: BlogPost[] = [
 	{
 		id: "coming-soon",
 		title: "Coming soon",
-		date: "October 2024",
+		date: "October 12, 2024",
 		content: `Coming soon, to a Denny's near you!`,
 	},
 ];
