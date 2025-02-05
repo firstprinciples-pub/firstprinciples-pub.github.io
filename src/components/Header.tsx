2d457b1964a6840b579f9f66fc4812b3aa0f3e91
ml5885 <limichael353@gmail.com>
2025-02-05 13:29:34 -0500

    style: added button hover animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,15 +9,7 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ minimized = false }) => (
 	<header className={minimized ? "header minimized" : "header"}>
 		<h1 className="header-title">
-			<Link
-				to="/"
-				style={{
-					textDecoration: minimized ? "underline" : "none",
-					color: "black",
-				}}
-			>
-				First Principles
-			</Link>
+			<Link to="/">First Principles</Link>
 		</h1>
 	</header>
 );
